83f70b2c5f9d1cad76dd59390279aabc4be34061
marciofao <marcio.lopes.fao@gmail.com>
2023-04-01 09:33:14 -0300

    setup testing

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -6,9 +6,11 @@ use PHPUnit\Framework\TestCase;
 
 final class ProductTest extends TestCase {
     public function testInsertProduct(): void {
-
+        require_once('vendor/autoload.php');
+        require_once('config.php');
+        require_once('database.php');
         require 'product_controller.php';
-        var_dump(insert_product('test_product', 2.2, 1));
+        var_dump(set_product('test_product', 2.2, 1));
         die;
         $this->assertEquals(
             get_product('test_product', 2.2, 1),
